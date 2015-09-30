71c3351cb6fa772bf21ca210aed1ddad4415e1f4
Marian Schubert <marian.schubert@gmail.com>
2015-09-30 13:06:48 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Clojure Intro
+
+![Examples](https://github.com/maio/clojure-intro/blob/master/src/intro/core.clj)
+
+## License
+
+Copyright © 2015 Marian Schubert
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/intro/core.clj
@@ -0,0 +1,53 @@
+(ns intro.core)
+
+;; http://clojure.org/data_structures
+
+;; Primitive data types ========================================================
+
+;; nil
+nil
+
+;; Numbers
+1
+2.3
+3/2345
+
+;; Boolean
+true
+false
+
+;; Strings
+"Hello World!"
+
+;; Keywords (very fast equality tests)
+:company
+
+;; Symbols (normally used to refer to something else)
+'blue
+
+;; Collections =================================================================
+
+;; Lists (linked list)
+'(1 2 3)
+
+;; Vectors (fast access to given index)
+[1 2 3]
+
+;; Maps (key/value pairs)
+{:name "John Doe"
+ :age 28}
+
+;; Sets
+#{1 2 3}
+
+;; Functions ===================================================================
+;; Many functions defined on few primary data structures.
+
+;; How do I call a function? Lisp...
+
+;; Destructuring
+
+;; do block
+
+;; Loops / Recursion
+
